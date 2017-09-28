9df7df43cd7f853d0f3b574b291e798350f98a02
Joel Francisco <joeld.fran@hotmail.com>
2017-09-28 21:56:29 +0100

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '',
     redirectTo: '/sign-up',
     pathMatch: 'full'
   }
 ];
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
