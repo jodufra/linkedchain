1ccc9d982a06d1b4726fbb113002879c503584ec
David Oliveira <david.oliveira@live.com>
2017-09-28 17:48:49 +0100

    sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
+
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+
+const appRoutes: Routes = [
+  { path: 'sign-up', component: SignUpComponent },
+  { path: '',
+    redirectTo: '/sign-up',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,11 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
