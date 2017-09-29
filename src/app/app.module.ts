d9caa59c5ea704eb734a82bb35131debff360b87
Joel Francisco <joel.francisco@zenn.pt>
2017-09-29 01:02:09 +0100

    profile, credit and certification components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CertificationComponent } from './certification/certification.component';
+import { CreditComponent } from './credit/credit.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const appRoutes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
@@ -20,7 +23,10 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     SignUpComponent,
-    DashboardComponent
+    DashboardComponent,
+    CertificationComponent,
+    CreditComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
