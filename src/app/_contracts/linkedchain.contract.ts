ff28fc2d95511a09e4d407856e66689444a110d8
Joel Francisco <joel.francisco@zenn.pt>
2017-09-29 02:42:09 +0100

    LinkedChain contract

--- src/app/_contracts/linkedchain.contract.ts
+++ src/app/_contracts/linkedchain.contract.ts
@@ -3,29 +3,58 @@ import { BlockchainService } from '../services/blockchain';
 import { Observable } from 'rxjs/Observable';
 
 export class LinkedChainContract implements IContract {
-  IBArray = [{ "constant": false, "inputs": [{ "name": "_address", "type": "address" }, { "name": "_name", "type": "string" }], "name": "createEntity", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_address", "type": "address" }], "name": "getEntity", "outputs": [{ "name": "", "type": "string" }, { "name": "", "type": "bool" }, { "name": "", "type": "bool" }], "payable": false, "type": "function" }];
+
+  IBArray = [{ "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "uint256" }], "name": "certifications", "outputs": [{ "name": "", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_name", "type": "string" }], "name": "updateEntity", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "_certification", "type": "string" }], "name": "addCertification", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "registedEntities", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getEntity", "outputs": [{ "name": "name", "type": "string" }, { "name": "isCompany", "type": "bool" }, { "name": "certificationsCounter", "type": "uint256" }], "payable": false, "type": "function" }, { "inputs": [], "payable": false, "type": "constructor" }];
   Address = "INSERT CONTRACT ADDRESS HERE";
 
+	/**
+	 * Teste constructor
+	 * @param {BlockchainService} blockchainService
+	 */
   constructor(private blockchainService: BlockchainService) {
-
   }
 
 	/**
-	 * createEntity function
-	 * @param {number} _address
+	 * certifications function
+	 * @param {number}
+	 * @param {number}
+	 * @returns {Observable< string  >}
+	 */
+  certifications(address_1: number, int_2: number): Observable<string> {
+    return this.blockchainService.executeMethod(this, 'certifications', { address_1, int_2 });
+  }
+
+	/**
+	 * updateEntity function
 	 * @param {string} _name
 	 */
-  createEntity(_address: number, _name: string) {
-    return this.blockchainService.executeMethod(this, 'createEntity', { _address, _name });
+  updateEntity(_name: string) {
+    return this.blockchainService.executeMethod(this, 'updateEntity', _name);
+  }
+
+	/**
+	 * addCertification function
+	 * @param {string} _certification
+	 */
+  addCertification(_certification: string) {
+    return this.blockchainService.executeMethod(this, 'addCertification', _certification);
+  }
+
+	/**
+	 * registedEntities function
+	 * @param {number}
+	 * @returns {Observable< boolean  >}
+	 */
+  registedEntities(address_1: number): Observable<boolean> {
+    return this.blockchainService.executeMethod(this, 'registedEntities', address_1);
   }
 
 	/**
 	 * getEntity function
-	 * @param {number} _address
-	 * @returns {Observable<{:string,:boolean,:boolean}>}
+	 * @returns {Observable<{ name : string, isCompany : boolean, certificationsCounter : number }>}
 	 */
-  getEntity(_address: number): Observable<{ a: string, b: boolean, c: boolean }> {
-    return this.blockchainService.executeMethod(this, 'getEntity', _address);
+  getEntity(): Observable<{ name: string, isCompany: boolean, certificationsCounter: number }> {
+    return this.blockchainService.executeMethod(this, 'getEntity', null);
   }
 
 }
