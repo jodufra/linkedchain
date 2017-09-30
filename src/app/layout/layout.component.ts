6c7052c4d3f4200ddbab78a74058b0aa300237ba
Marco Ramos <mail.marcoramos@gmail.com>
2017-09-30 12:17:51 +0100

    toggle menu to assign only to companies

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { SessionService } from '../_shared/services/session';
+import { Entity } from '../_shared/entities/entity';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styles: ['./layout.component.css']
 })
-export class LayoutComponent {
- 
+export class LayoutComponent implements OnInit {
+
+   entity: Entity = null;
+
   constructor(
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private ref: ChangeDetectorRef
   ) {
     this.sessionService.load();
   }
 
+  ngOnInit(): void {
+    this.entity = this.sessionService.entity;
+    this.sessionService.sessionUpdated.subscribe((entity: Entity) => { 
+      this.entity = entity; 
+      this.ref.detectChanges();
+    } );
+  }
 }
